1ad5309818fff864a9836f3d2ff1e2e6ce6c3007
ILYA ERSHOV <ershov.ilya@gmail.com>
2017-04-17 11:20:56 +0300

    Работают 3 метода

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ function genArray (strlen=8, arrsize=3, forceDuplicate=false) {
 	return arr
 }
 
-var sha1 = require("crypto-js/sha1");
+var md5 = require("crypto-js/md5");
 var hash_method = (str) => {
-	return sha1(str).toString()
+	return md5(str).toString()
 }
 
 
@@ -84,32 +84,56 @@ function hasDupplicate_compareEach(arr){
 /**
 * Метрод проверки построением индекса
 */
-function Node(value, wordEnd=false){
+function Node(value, wordEnd=false, prev){
 	this.value=value
+	//this.prev=prev
 	this.wordEnd=wordEnd
 	this.branches={}
 }
 
 function Index(){
-	let root={
+	this.root={
 		branches: {}
 	}
 
 	this.add = (str) =>{
-		let i,last=str.length-1,ptr=root
+		let i,current,last=str.length-1,ptr=this.root
+        
+        // Перебор символов в строке
 		for(i=0; i<str.length; i++){
-			let node=new Node(str[i], (i==last?true:false))
-			
+            // Проверяем создана ли  ветка
+            if(typeof ptr.branches[str[i]]=='undefined'){
+                // если нет, то создаём новую
+                let node=new Node(str[i], (i==last?1:0), ptr)
+                ptr.branches[str[i]]=node;
+                ptr=node;
+                //console.log('Новый узел', node)
+            } else {
+                current=ptr.branches[str[i]]
+                if(i==last){
+                    current.wordEnd++
+                }
+                if(current.wordEnd>1) throw {message: 'Совпали концы', found_duplicate: true}
+                ptr=current
+            }
 		}
-		console.log(str)
+		// console.log(str)
 	}
 }
 
 function hasDupplicate_indexMethod(arr){
-	let time = new Date().getTime();
-
+	let found_duplicate=false, time = new Date().getTime();
+    
+    let index = new Index()
 	/* Рабочий рассчёт */
-	/* ... */
+	let i,arrlen=arr.length
+    try{
+        for(i=0; i<arrlen; i++){
+            index.add(arr[i])
+        }
+    }catch(obj){
+        if(obj.found_duplicate) found_duplicate=true
+    }
 
 	time = new Date().getTime() - time
 	return {
@@ -130,20 +154,30 @@ let main = () => {
 		indexMethod: 0
 	}
 
-	let i,res,arr,props,program=[
+    // переменные
+	let i,res,arr,props,program
+    
+    // Генерируем массивы для тестирования
+    program=[
 		{strlen:8, arrsize:10},
-	    {strlen:8, arrsize:30},
-	    {strlen:8, arrsize:100},
+	    //{strlen:8, arrsize:30},
+	    //{strlen:8, arrsize:100},
 	    //{strlen:32, arrsize:100},
 	    //{strlen:64, arrsize:10000},
     ]
-
-    /*
-    // Замеряем методом с хэшами
-    console.log('Замеряем методом с хэшами:')
-	for(i=0; i<program.length; i++){
+    
+    tasks=[] // Набор массивов для тестирования
+    for(i=0; i<program.length; i++){
 		props=program[i]
 		arr=genArray(props.strlen, props.arrsize, true)
+        tasks.push(arr)
+    }
+
+    
+    // Замеряем методом с хэшами
+    console.log('Замеряем методом с хэшами:')
+	for(i=0; i<tasks.length; i++){
+		arr=tasks[i]
 		
 		// Замеряем методом с хэшами
 		res=hasDupplicate_hashMethod(arr)
@@ -154,9 +188,8 @@ let main = () => {
 
     // Замеряем методом сравнением
     console.log('Замеряем методом сравнением:')
-	for(i=0; i<program.length; i++){
-		props=program[i]
-		arr=genArray(props.strlen, props.arrsize, true)
+	for(i=0; i<tasks.length; i++){
+		arr=tasks[i]
 		
 		// Замеряем методом с хэшами
 		res=hasDupplicate_compareEach(arr)
@@ -164,12 +197,20 @@ let main = () => {
 		result.compareMethod+=res.time
 	}
 	console.log(result)
-	*/
+    
+    // Замеряем методом индексирования
+    console.log('Замеряем методом индексирования:')
+	for(i=0; i<tasks.length; i++){
+		arr=tasks[i]
+		
+		// Замеряем методом с хэшами
+		res=hasDupplicate_indexMethod(arr)
+		console.log((i+1)+') ',res)
+		result.indexMethod+=res.time
+	}
+	console.log(result)
 
-	let index = new Index()
-	index.add('bjdanwdi')
-	index.add('owninwn')
-	index.add('bjdanwdi')
+
 
 	console.timeEnd('Test total')
 }
